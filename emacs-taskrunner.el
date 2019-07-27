069609caa2abc4f6efdb385b5dcc5e93973aca04
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-07-27 16:13:36 -0700

    Add outline to extract gulp tasks

--- emacs-taskrunner.el
+++ emacs-taskrunner.el
@@ -2,6 +2,9 @@
 
 (require 'projectile)
 
+(defconst taskrunner--js-gulp-tasks-command "gulp --tasks-json"
+  "Command used to retrieve the tasks for 'gulp' in json form.")
+
 (defun taskrunner--js-get-package-tasks ()
   "Open and extract the tasks from package.json.
 This command returns a list containing the names of the tasks as strings."
@@ -13,5 +16,16 @@ This command returns a list containing the names of the tasks as strings."
     (dolist (el (cdr package-json-contents))
       (setq package-tasks (push (symbol-name (car el)) package-tasks)))
     (message "%s" package-tasks)
+    )
+  )
+
+(defun taskrunner--js-get-gulp-tasks ()
+  "Retrieve tasks for gulp if the file is found.
+If no file exists, return an empty list."
+  (interactive)
+  (let ((default-directory (projectile-project-root))
+        (gulp-json-tasks (cdr (cadr (json-read-from-string (shell-command-to-string taskrunner--js-gulp-tasks-command)))))
+        )
+    (message "%s" gulp-json-tasks)
     )
   )
