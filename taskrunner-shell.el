5b561b586ae958a6c8f54d85255ea071612e36b8
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-08-13 15:24:16 -0700

    Add functions to retrieve shell script names

--- taskrunner-shell.el
+++ taskrunner-shell.el
@@ -1,10 +1,33 @@
 ;; Files related to finding shell/powershell scripts and returning their names
 ;; for selection
 
-(defun taskrunner--get-shell-scripts (dir)
-  "Return the names of all shell scripts in directory DIR.")
+(defun taskrunner--get-scripts (dir filetype prefix)
+  "Return a list of all scripts matching regexp FILETYPE in directory DIR.
+All names are prefixed with the string provided by PREFIX."
+  (if (directory-name-p dir)
+      (progn
+        (let ((shell-scripts (directory-files dir nil filetype)))
+          (map 'list (lambda (elem)
+                       (concat prefix " " elem))
+               shell-scripts)
+          )
+        )
+    nil
+    )
+  )
+
+(taskrunner--get-shell-scripts "~/pprojects/em-taskrunner/")
+
+(defun taskrunner--get-bash-scripts (dir)
+  "Retrieve all bash scripts in directory DIR."
+  (taskrunner--get-scripts dir "\\.sh$" "BASH"))
 
 (defun taskrunner--get-powershell-scripts (dir)
-  "Return the names of all powershell scripts in directory DIR.")
+  "Retrieve all powershell scripts in directory DIR."
+  (taskrunner--get-scripts dir "\\.ps1$" "POWERSHELL"))
+
+(defun taskrunner--get-zsh-scripts (dir)
+  "Retrieve all zsh scripts in directory DIR"
+  (taskrunner--get-scripts dir "\\.zsh$" "ZSH"))
 
 (provide 'taskrunner-shell)
