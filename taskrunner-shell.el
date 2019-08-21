108034303a9583ac0742f17d44523106c9912b1b
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-08-21 09:38:17 -0700

    Fix shell bytecompile. Add docs

--- taskrunner-shell.el
+++ taskrunner-shell.el
@@ -1,31 +1,38 @@
-;; Files related to finding shell/powershell scripts and returning their names
-;; for selection
+;;; taskrunner-shell.el --- Provide functions to retrieve shell script targets -*- lexical-binding: t; -*-
+;; Copyright (C) 2019 Yavor Konstantinov
 
-(defun taskrunner--get-scripts (dir filetype-regexp prefix)
+;;;; Commentary:
+;; Support for:
+;; - Bash
+;; - Powershell
+;; - Zsh
+
+;;;; Code:
+
+;;;; Functions
+
+(defun taskrunner--get-scripts (DIR FILETYPE-REGEXP PREFIX)
   "Return a list of all scripts matching regexp FILETYPE-REGEXP in DIR.
 All names are prefixed with the string provided by PREFIX."
-  (if (directory-name-p dir)
+  (if (directory-name-p DIR)
       (progn
-        (let ((shell-scripts (directory-files dir nil filetype-regexp)))
+        (let ((shell-scripts (directory-files DIR nil FILETYPE-REGEXP)))
           (map 'list (lambda (elem)
-                       (concat prefix " " elem))
-               shell-scripts)
-          )
-        )
-    nil
-    )
-  )
+                       (concat PREFIX " " elem))
+               shell-scripts)))
+    nil))
 
-(defun taskrunner--get-bash-scripts (dir)
+(defun taskrunner--get-bash-scripts (DIR)
   "Retrieve all bash scripts in directory DIR."
-  (taskrunner--get-scripts dir "\\.sh$" "BASH"))
+  (taskrunner--get-scripts DIR "\\.sh$" "BASH"))
 
-(defun taskrunner--get-powershell-scripts (dir)
+(defun taskrunner--get-powershell-scripts (DIR)
   "Retrieve all powershell scripts in directory DIR."
-  (taskrunner--get-scripts dir "\\.ps1$" "POWERSHELL"))
+  (taskrunner--get-scripts DIR "\\.ps1$" "POWERSHELL"))
 
-(defun taskrunner--get-zsh-scripts (dir)
-  "Retrieve all zsh scripts in directory DIR"
-  (taskrunner--get-scripts dir "\\.zsh$" "ZSH"))
+(defun taskrunner--get-zsh-scripts (DIR)
+  "Retrieve all zsh scripts in directory DIR."
+  (taskrunner--get-scripts DIR "\\.zsh$" "ZSH"))
 
 (provide 'taskrunner-shell)
+;;; taskrunner-shell.el ends here
