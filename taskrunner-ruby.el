ae03d0cbbbea82e48e0de25b18b36d9045bbc57f
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-08-29 15:19:42 -0700

    Fix ruby task function

--- taskrunner-ruby.el
+++ taskrunner-ruby.el
@@ -6,20 +6,32 @@
 
 ;;;; Code:
 (require 'cl-lib)
-
-;;;; Variables
-(defconst taskrunner--rake-tasks-command '("rake" "-AT")
-  "Command used to retrieve the tasks from rake.")
+(require 'taskrunner)
 
 ;;;; Functions
+
+(defun taskrunner--retrieve-rake-tasks-from-buffer ()
+  "Retrieve the rake tasks from the current buffer.
+This function returns a list of the form:
+\(\"RAKE TASK1\" \"RAKE TASK2\"...)"
+  (goto-char (point-min))
+  (let ((rake-tasks '()))
+    (while (search-forward-regexp "^rake[[:space:]]+" nil t)
+      (taskrunner--narrow-to-line)
+      (push (concat "RAKE" " " (cadr (split-string (buffer-string) " " t))) rake-tasks)
+      (widen))
+    rake-tasks))
+
+
 (defun taskrunner-get-rake-tasks (DIR)
-  "Retrieve the rake tasks for the project in directory DIR.
+  "Retrieve the ant tasks for the project in directory DIR.
 This function returns a list of the form:
 \(\"RAKE TASK1\" \"RAKE TASK2\"...)"
   (let ((default-directory DIR))
-    (cl-map 'list (lambda (elem)
-                    (concat "RAKE" " " (cadr (split-string elem " "))))
-            (split-string (shell-command-to-string taskrunner--rake-tasks-command) "\n"))))
+    (call-process "rake"  nil (taskrunner--make-task-buff-name "rake")  nil "-AT")
+    (with-temp-buffer
+      (set-buffer (taskrunner--make-task-buff-name "rake"))
+      (taskrunner--retrieve-rake-tasks-from-buffer))))
 
 (provide 'taskrunner-ruby)
 ;;; taskrunner-ruby.el ends here
